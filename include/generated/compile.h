04f4645a6701f1d2a6ed366f8192297adc4c2322
HerokeTkz <bladetrinity466@gmail.com>
2024-12-19 00:04:45 -0300

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Dec 13 14:33:50 -03 2024"
+#define LINUX_COMPILE_BY "herokezin"
+#define LINUX_COMPILE_HOST "herokezin-HP-EliteBook-2560p"
+#define LINUX_COMPILER "Android (6875598, based on r399163b) clang version 11.0.5 (https://android.googlesource.com/toolchain/llvm-project 87f1315dfbea7c137aa2e6d362dbb457e388158d)"
